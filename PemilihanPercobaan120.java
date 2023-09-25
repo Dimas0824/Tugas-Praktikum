944e4aae293f3b8b4bd85ed3d7da9bf691aa9200
Dimas0824 <dimassmadapas@gmail.com>
2023-09-25 13:16:37 +0700

    versi ternary operator

--- PemilihanPercobaan120.java
+++ PemilihanPercobaan120.java
@@ -5,11 +5,8 @@ public class PemilihanPercobaan120{
         Scanner input20 = new Scanner(System.in);
         System.out.print("Masukkan angka: ");
         int angka20 = input20.nextInt();
-        if (angka20 % 2 == 0) {
-            System.out.println("Angka " + angka20 + " adalah bilangan genap");
-        } else {
-            System.out.println("Angka " + angka20 + " adalah bilangan ganjil");
-        }
-        }
         
+        String status = (angka20 % 2 == 0) ? "genap" : "ganjil";
+        System.out.println("Angka " + angka20 + " adalah bilangan " + status);
+    }
 }
